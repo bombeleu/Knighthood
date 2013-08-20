c1f0aee33235d66ac9130cfc2070474dab9522f0
Steve Yeager <steve.f.yeager@gmail.com>
2013-08-19 23:58:48 -0700

    CameraInfo class.
    Holds data for the Level Manager camera.

---
+++ Knighthood Project/Assets/Code/Game/Scene/CameraInfo.cs
@@ -0,0 +1,19 @@
+﻿// Steve Yeager
+// 8.19.2013
+
+using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class CameraInfo
+{
+  /// <summary></summary>
+  public float boundaryDynamic;
+  /// <summary></summary>
+  public float boundaryStatic;
+  [HideInInspector]
+  /// <summary></summary>
+  public bool locked;
+
+} // end CameraInfo class
