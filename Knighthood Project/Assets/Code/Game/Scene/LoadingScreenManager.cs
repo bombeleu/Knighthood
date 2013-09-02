8cecfcea791859ba5966f4861217e256d3afba39
Steve Yeager <steve.f.yeager@gmail.com>
2013-09-01 19:38:57 -0700

    Added Tips and got rid of To Do

--- Knighthood Project/Assets/Code/Game/Scene/LoadingScreenManager.cs
+++ Knighthood Project/Assets/Code/Game/Scene/LoadingScreenManager.cs
@@ -1,11 +1,11 @@
 ﻿// Steve Yeager
-// 
+// 9.1.2013
 
 using UnityEngine;
 using System.Collections;
 
 /// <summary>
-/// 
+/// Loads the next scene and displays loading images.
 /// </summary>
 public class LoadingScreenManager : MonoBehaviour
 {
