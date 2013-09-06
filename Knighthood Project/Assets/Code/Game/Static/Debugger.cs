50bf4d602387d813457c1d9f8a42ef0e76b722d4
Steve Yeager <steve.f.yeager@gmail.com>
2013-09-05 22:47:30 -0700

    Organized.

--- Knighthood Project/Assets/Code/Game/Static/Debugger.cs
+++ Knighthood Project/Assets/Code/Game/Static/Debugger.cs
@@ -9,51 +9,54 @@ using System.Collections;
 /// </summary>
 public static class Debugger
 {
-  public static void Log(object message)
-  {
-    Debug.Log(message);
-  } // end Log
+    #region Log Methods
+    public static void Log(object message)
+    {
+        Debug.Log(message);
+    } // end Log
 
 
-  public static void Log(object message, Object context)
-  {
-    Debug.Log(message, context);
-  } // end Log
+    public static void Log(object message, Object context)
+    {
+        Debug.Log(message, context);
+    } // end Log
 
 
-  public static void LogWarning(object message)
-  {
-    Debug.LogWarning(message);
-  } // end LogWarning
+    public static void LogWarning(object message)
+    {
+        Debug.LogWarning(message);
+    } // end LogWarning
 
 
-  public static void LogWarning(object message, Object context)
-  {
-    Debug.LogWarning(message, context);
-  } // end LogWarning
+    public static void LogWarning(object message, Object context)
+    {
+        Debug.LogWarning(message, context);
+    } // end LogWarning
 
 
-  public static void LogError(object message)
-  {
-    Debug.LogError(message);
-  } // end LogError
+    public static void LogError(object message)
+    {
+        Debug.LogError(message);
+    } // end LogError
 
 
-  public static void LogError(object message, Object context)
-  {
-    Debug.LogError(message, context);
-  } // end LogError
+    public static void LogError(object message, Object context)
+    {
+        Debug.LogError(message, context);
+    } // end LogError
 
 
-  public static void LogException(System.Exception exception)
-  {
-    Debug.LogException(exception);
-  } // end LogException
+    public static void LogException(System.Exception exception)
+    {
+        Debug.LogException(exception);
+    } // end LogException
 
 
-  public static void LogException(System.Exception exception, Object context)
-  {
-    Debug.LogException(exception, context);
-  } // end LogException
+    public static void LogException(System.Exception exception, Object context)
+    {
+        Debug.LogException(exception, context);
+    } // end LogException
+
+    #endregion
 
 } // end Debugger class
