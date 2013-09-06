552e0d4e2f57a3feaa2b8b29f818a91cfcab4a8b
Steve Yeager <steve.f.yeager@gmail.com>
2013-09-05 22:48:38 -0700

    PlayerHUD foundation.
    Mostly temp.

---
+++ Knighthood Project/Assets/Code/Game/GUI/PlayerHUD.cs
@@ -0,0 +1,37 @@
+﻿// Steve Yeager
+// 9.5.2013
+
+using UnityEngine;
+
+/// <summary>
+/// HUD for player.
+/// </summary>
+public class PlayerHUD : BaseMono
+{
+    private Player player;
+    private Health health;
+    private Magic magic;
+
+
+    #region MonoBehaviour Overrides
+
+    private void Awake()
+    {
+        // get references
+        player = GetComponent<Player>();
+        health = GetComponent<Health>();
+        magic = GetComponent<Magic>();
+    }
+
+
+    private void OnGUI()
+    {
+        // temp
+        GUILayout.Label(player.playerInfo.username);
+        GUILayout.Label("Health: " + health.currentHealth);
+        GUILayout.Label("Magic: " + magic.currentMagic);
+    }
+
+    #endregion
+
+}
