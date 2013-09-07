3117282bcc83c19734bf97fdbbd6965f3119b6b0
Steve Yeager <steve.f.yeager@gmail.com>
2013-09-06 23:06:39 -0700

    Align transform to camera by inverting scale.x

---
+++ Knighthood Project/Assets/Code/Extensions/TransformExtension.cs
@@ -0,0 +1,16 @@
+﻿// Steve Yeager
+// 9.6.2013
+
+using UnityEngine;
+
+/// <summary>
+/// Extend the Transform class.
+/// </summary>
+public static class TransformExtension
+{
+    public static void Align(this Transform transform)
+    {
+        transform.localScale = new Vector3(Vector3.Dot(transform.forward, Vector3.right) * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+}
