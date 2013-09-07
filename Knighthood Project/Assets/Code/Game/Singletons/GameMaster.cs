4443e81f5da74729de48986710012bada4ba0396
Steve Yeager <steve.f.yeager@gmail.com>
2013-09-06 23:08:17 -0700

    Only one GameMaster allowed.

---
+++ Knighthood Project/Assets/Code/Game/Singletons/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿// Steve Yeager
+// 9.6.2013
+
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class GameMaster : BaseMono
+{
+    private void Awake()
+    {
+        if (GameObject.FindGameObjectsWithTag("GameMaster").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+}
